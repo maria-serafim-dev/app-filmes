5ed1564dcecd153fe4cfd7b3c3c1663f846cfa51
maria-serafim-dev <maria.serafim.dev@gmail.com>
2022-03-14 20:03:34 -0300

    Adicionando ação de click nos filmes listados no AssistaTambemFragment.kt e MinhaListaFragment.kt

--- app/src/main/java/com/example/appdefilmes/fragments/AssistaTambemFragment.kt
+++ app/src/main/java/com/example/appdefilmes/fragments/AssistaTambemFragment.kt
@@ -1,5 +1,6 @@
 package com.example.appdefilmes.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,7 +8,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.appdefilmes.R
+import com.example.appdefilmes.activity.InformacoesFilmeActivity
 import com.example.appdefilmes.adapters.recyclerview.adapter.FilmeAdapter
+import com.example.appdefilmes.adapters.recyclerview.adapter.InterfaceOnClick
 import com.example.appdefilmes.dao.FilmeDAO
 import com.example.appdefilmes.model.Filme
 import com.example.appdefilmes.retrofit.FilmeResponse
@@ -37,7 +40,19 @@ class AssistaTambemFragment : Fragment() {
 
     fun adaptarRecycleView(view: View, id: Int, filmes: List<Filme>){
         val recyclerView: RecyclerView = view.findViewById(id)
-        recyclerView.adapter = filmes.let { FilmeAdapter(view.context, it) }
+        val adapter = filmes.let { FilmeAdapter(view.context, it) }
+        recyclerView.adapter = adapter
+        adapter.setOnClick(object: InterfaceOnClick {
+            override fun onItemClick(filme: Filme) {
+                telaInformacaoFilme(filme, view)
+            }
+        })
+    }
+
+    private fun telaInformacaoFilme(filme: Filme, view: View) {
+        val intent = Intent(view.context, InformacoesFilmeActivity::class.java)
+        intent.putExtra("filme", filme)
+        startActivity(intent)
     }
 
 
