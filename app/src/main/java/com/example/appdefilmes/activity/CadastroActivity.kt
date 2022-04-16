830a67dba9a91062b51dc3ebd0947d84e1f225d6
maria-serafim-dev <maria.serafim.dev@gmail.com>
2022-04-16 15:38:48 -0300

    Input de estados brasileiros com DropdownMenu

--- app/src/main/java/com/example/appdefilmes/activity/CadastroActivity.kt
+++ app/src/main/java/com/example/appdefilmes/activity/CadastroActivity.kt
@@ -1,11 +1,9 @@
 package com.example.appdefilmes.activity
 
-import android.os.Binder
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.appcompat.app.AppCompatActivity
 import com.example.appdefilmes.R
 import com.example.appdefilmes.databinding.ActivityCadastroBinding
 
@@ -19,7 +17,15 @@ class CadastroActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val items = listOf("Feminino", "Masculino", "Outro", "Não quero informar")
-        val adapter = ArrayAdapter(applicationContext, R.layout.lista_item_input, items)
-        (binding.tfGenero.editText as? AutoCompleteTextView)?.setAdapter(adapter)
+        val estadosBrasileiros = listOf("Acre (AC)", "Alagoas (AL)","Amapá (AP)","Amazonas (AM)","Bahia (BA)","Ceará (CE)", "Distrito Federal (DF)","Espírito Santo (ES)", "Goiás (GO)",
+            "Maranhão (MA)","Mato Grosso (MT)","Mato Grosso do Sul (MS)","Minas Gerais (MG)","Pará (PA)","Paraíba (PB)","Paraná (PR)","Pernambuco (PE)",
+            "Piauí (PI)","Rio de Janeiro (RJ)","Rio Grande do Norte (RN)","Rio Grande do Sul (RS)","Rondônia (RO)","Roraima (RR)","Santa Catarina (SC)","São Paulo (SP)",
+            "Sergipe (SE)","Tocantins (TO)")
+
+        val adapterGenero = ArrayAdapter (applicationContext, R.layout.lista_item_input, items)
+        (binding.tfGenero.editText as? AutoCompleteTextView)?.setAdapter(adapterGenero)
+
+        val adapterEstados = ArrayAdapter (applicationContext, R.layout.lista_item_input, estadosBrasileiros)
+        (binding.tfEstado.editText as? AutoCompleteTextView)?.setAdapter(adapterEstados)
     }
 }
