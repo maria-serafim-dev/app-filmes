aa9d9bae35699fbec1ed1493c9c5fa632ed61952
maria-serafim-dev <maria.serafim.dev@gmail.com>
2022-04-16 16:05:18 -0300

    Refatoração dos dados dos inputs

--- app/src/main/java/com/example/appdefilmes/activity/CadastroActivity.kt
+++ app/src/main/java/com/example/appdefilmes/activity/CadastroActivity.kt
@@ -5,6 +5,8 @@ import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.appdefilmes.R
+import com.example.appdefilmes.data.itensEstadosBrasileiros
+import com.example.appdefilmes.data.itensGenero
 import com.example.appdefilmes.databinding.ActivityCadastroBinding
 
 class CadastroActivity : AppCompatActivity() {
@@ -15,17 +17,16 @@ class CadastroActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCadastroBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        iniciarInputsDropdown()
 
-        val items = listOf("Feminino", "Masculino", "Outro", "Não quero informar")
-        val estadosBrasileiros = listOf("Acre (AC)", "Alagoas (AL)","Amapá (AP)","Amazonas (AM)","Bahia (BA)","Ceará (CE)", "Distrito Federal (DF)","Espírito Santo (ES)", "Goiás (GO)",
-            "Maranhão (MA)","Mato Grosso (MT)","Mato Grosso do Sul (MS)","Minas Gerais (MG)","Pará (PA)","Paraíba (PB)","Paraná (PR)","Pernambuco (PE)",
-            "Piauí (PI)","Rio de Janeiro (RJ)","Rio Grande do Norte (RN)","Rio Grande do Sul (RS)","Rondônia (RO)","Roraima (RR)","Santa Catarina (SC)","São Paulo (SP)",
-            "Sergipe (SE)","Tocantins (TO)")
+    }
 
-        val adapterGenero = ArrayAdapter (applicationContext, R.layout.lista_item_input, items)
+    private fun iniciarInputsDropdown() {
+        val adapterGenero = ArrayAdapter(applicationContext, R.layout.lista_item_input, itensGenero)
         (binding.tfGenero.editText as? AutoCompleteTextView)?.setAdapter(adapterGenero)
 
-        val adapterEstados = ArrayAdapter (applicationContext, R.layout.lista_item_input, estadosBrasileiros)
+        val adapterEstados =
+            ArrayAdapter(applicationContext, R.layout.lista_item_input, itensEstadosBrasileiros)
         (binding.tfEstado.editText as? AutoCompleteTextView)?.setAdapter(adapterEstados)
     }
 }
