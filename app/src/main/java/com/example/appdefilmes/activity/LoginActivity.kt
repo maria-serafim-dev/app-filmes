7ea1b60a734fe7171293fb66f9c15b05aaab3dde
maria-serafim-dev <maria.serafim.dev@gmail.com>
2022-04-19 19:20:28 -0300

    Login com e-mail e senha.

--- app/src/main/java/com/example/appdefilmes/activity/LoginActivity.kt
+++ app/src/main/java/com/example/appdefilmes/activity/LoginActivity.kt
@@ -1,13 +1,51 @@
 package com.example.appdefilmes.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.example.appdefilmes.R
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.appdefilmes.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ouvinteBotaoLogin()
     }
+
+    private fun ouvinteBotaoLogin() {
+        binding.btnEntrar.setOnClickListener {
+            val email = binding.editEmail.text.toString()
+            val senha = binding.editSenha.text.toString()
+
+            auth.signInWithEmailAndPassword(email, senha).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.i("signIn", "Sucesso ao logar usuário")
+                    nextActivity()
+                } else {
+                    messageErro("e-mail e senha")
+                    Log.i("signIn", "Erro ao logar usuário", task.exception)
+                }
+            }
+        }
+    }
+
+    fun nextActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun messageErro(provedor: String) {
+        Toast.makeText(this, "Falha na autenticação com $provedor",Toast.LENGTH_LONG).show()
+    }
+
 }
