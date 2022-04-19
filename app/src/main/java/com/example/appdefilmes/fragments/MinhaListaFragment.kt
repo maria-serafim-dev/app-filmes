ac071d5a03dc1b2b282a01db1f1b94ed0b13d521
maria-serafim-dev <maria.serafim.dev@gmail.com>
2022-04-19 15:03:53 -0300

    Início da utilização do ViewModel com o LiveData

--- app/src/main/java/com/example/appdefilmes/fragments/MinhaListaFragment.kt
+++ app/src/main/java/com/example/appdefilmes/fragments/MinhaListaFragment.kt
@@ -5,20 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.appdefilmes.adapters.recyclerview.adapter.FilmeAdapter
 import com.example.appdefilmes.adapters.recyclerview.adapter.InterfaceOnClick
-import com.example.appdefilmes.dao.FilmeDAO
 import com.example.appdefilmes.databinding.FragmentMinhaListaBinding
 import com.example.appdefilmes.model.Filme
-import com.example.appdefilmes.retrofit.FilmeResponse
+import com.example.appdefilmes.viewModel.FilmeViewModel
 
 
 class MinhaListaFragment : Fragment() {
 
     private var _binding: FragmentMinhaListaBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: FilmeViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,21 +31,17 @@ class MinhaListaFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentMinhaListaBinding.inflate(inflater, container, false)
-        val view = binding.root
-        filmesPopulares(view)
-        return view
+        return binding.root
     }
 
-    private fun filmesPopulares(view: View){
-        FilmeDAO().getListaFavoritos(object: FilmeResponse {
-            override fun sucesso(filmes: List<Filme>) {
-                adaptarRecycleView(view, filmes)
-            }
-
-        })
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.filmesFavoritos.observe(viewLifecycleOwner) { listaFilme ->
+            adaptarRecycleView(view, listaFilme)
+        }
     }
 
-    private fun adaptarRecycleView(view: View, filmes: List<Filme>){
+    private fun adaptarRecycleView(view: View, filmes: List<Filme>) {
         val recyclerView: RecyclerView = binding.rvMinhaLista
         val adapter = FilmeAdapter(view.context, filmes)
         recyclerView.adapter = adapter
---
+++ app/src/main/java/com/example/appdefilmes/viewModel/FilmeViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.appdefilmes.viewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.appdefilmes.dao.FilmeDAO
+import com.example.appdefilmes.model.Filme
+import com.example.appdefilmes.retrofit.FilmeResponse
+
+class FilmeViewModel : ViewModel() {
+
+    private val _filmesFavoritos = MutableLiveData<List<Filme>>()
+    val filmesFavoritos: LiveData<List<Filme>>
+        get() = _filmesFavoritos
+
+    private fun filmesPopulares(){
+        FilmeDAO().getListaFavoritos(object: FilmeResponse {
+            override fun sucesso(filmes: List<Filme>) {
+                _filmesFavoritos.value = filmes
+            }
+        })
+    }
+
+    init {
+        filmesPopulares()
+    }
+
+}
